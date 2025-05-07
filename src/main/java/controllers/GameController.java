2f18ab144f58ff374348f6d80183e1c3a78d094c
zach <ke3n1@proton.me>
2025-05-06 23:57:14 -0400

    comments

--- src/main/java/controllers/GameController.java
+++ src/main/java/controllers/GameController.java
@@ -3,18 +3,5 @@ package controllers;
 import models.GameModel;
 
 public class GameController {
-    private final GameModel model;
-
-    public GameController(){
-
-    }
-    public GameController(GameModel model){
-        this.model = model;
-    }
-
-    public GameModel getModel(){
-        return this.model;
-    }
-
 
 }
--- src/main/java/models/GameModel.java
+++ src/main/java/models/GameModel.java
@@ -1,11 +1,16 @@
 package models;
 
+//game model class
+//contains the board and some summary stats regarding the game state
 public class GameModel {
+
+    //board and state attributes
     private Board board;
     private int flaggedCount;
     private int bombCount;
     private int unflaggedBombCount;
 
+    //constructors
     public GameModel(){}
     public GameModel(Board board){
         this.board = board;
@@ -13,20 +18,10 @@ public class GameModel {
 
 
 
-    public boolean hasBomb(int row, int col){
-        return this.getBoard().getCell(row, col).hasBomb();
-    }
-    public boolean hasUserFlag(int row, int col){
-        return this.getBoard().getCell(row, col).hasUserFlag();
-    }
-    public void setBomb(int row, int col, boolean bomb){
-        this.getBoard().getCell(row, col).setBomb(bomb);
-    }
-    public void setUserFlag(int row, int col, boolean userFlag){
-        this.getBoard().getCell(row, col).setUserFlag(userFlag);
-    }
 
+    //update the count for the current number of flagged cells
     public void updateFlaggedCount(){
+
         int flagged = this.getFlaggedCount();
         for(int i = 0; i < this.getBoard().getCells().length; i++){
             for(int j = 0; j < this.getBoard().getCells()[i].length; j++){
@@ -38,6 +33,7 @@ public class GameModel {
         this.setFlaggedCount(flagged);
     }
 
+    //update the count for the current number of bombs not flagged by the user
     public void updateUnflaggedBombCount(){
         int unflaggedBombs = this.getUnflaggedBombCount();
         for(int i = 0; i < this.getBoard().getCells().length; i++){
@@ -50,18 +46,41 @@ public class GameModel {
         this.setUnflaggedBombCount(unflaggedBombs);
     }
 
+    //get the bomb status of a particular cell
+    public boolean hasBomb(int row, int col){
+        return this.getBoard().getCell(row, col).hasBomb();
+    }
+
+    //get the flagged status of a particular cell
+    public boolean hasUserFlag(int row, int col){
+        return this.getBoard().getCell(row, col).hasUserFlag();
+    }
+
+    //set the bomb status of a particular cell
+    public void setBomb(int row, int col, boolean bomb){
+        this.getBoard().getCell(row, col).setBomb(bomb);
+    }
+
+    //set the flagged status of a particular cell
+    public void setUserFlag(int row, int col, boolean userFlag){
+        this.getBoard().getCell(row, col).setUserFlag(userFlag);
+    }
+
+    //reset the board with the current row and column conditions
     public void resetBoard(){
+
         int row = this.getBoard().getCells().length;
         int col = this.getBoard().getCells()[row].length;
+
         Cell[][] cells = new Cell[row][col];
-        Board board = new Board();
-        board.setCells(cells);
+        Board board = new Board(cells);
         this.setBoard(board);
     }
+
+    //reset the board with new row and column conditions
     public void resetBoard(int row, int col){
         Cell[][] cells = new Cell[row][col];
-        Board board = new Board();
-        board.setCells(cells);
+        Board board = new Board(cells);
         this.setBoard(board);
     }
 
