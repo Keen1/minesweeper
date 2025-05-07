374b2ca4435c73a3877275e536da0993d0507286
zach <ke3n1@proton.me>
2025-05-07 00:37:13 -0400

    more work on the model

--- src/main/java/models/GameModel.java
+++ src/main/java/models/GameModel.java
@@ -50,63 +50,6 @@ public class GameModel {
         this.setUnflaggedBombCount(unflaggedBombs);
     }
 
-    //get the bomb status of a particular cell
-    public boolean hasBomb(int row, int col){
-        return this.getBoard().getCell(row, col).hasBomb();
-    }
-
-    //get the flagged status of a particular cell
-    public boolean hasUserFlag(int row, int col){
-        return this.getBoard().getCell(row, col).hasUserFlag();
-    }
-
-    //set the bomb status of a particular cell
-    public void setBomb(int row, int col, boolean bomb){
-        this.getBoard().getCell(row, col).setBomb(bomb);
-    }
-
-    //set the flagged status of a particular cell
-    public void setUserFlag(int row, int col, boolean userFlag){
-        this.getBoard().getCell(row, col).setUserFlag(userFlag);
-    }
-
-    //reset the board with the current row and column conditions
-    public void resetBoard(){
-
-        int row = this.getBoard().getCells().length;
-        int col = this.getBoard().getCells()[row].length;
-
-        Cell[][] cells = new Cell[row][col];
-        Board board = new Board(cells);
-        this.setBoard(board);
-    }
-
-    //reset the board with new row and column conditions
-    public void resetBoard(int row, int col){
-        Cell[][] cells = new Cell[row][col];
-        Board board = new Board(cells);
-        this.setBoard(board);
-    }
-
-    //set a given number of bombs on the board given a count
-    public void setBombs(int count){
-
-        //need to generate two random integers, the row value and the column value
-        SecureRandom secureRand = new SecureRandom();
-        int maxRows = this.getBoard().getCells().length;
-        int maxCols = this.getBoard().getCells()[maxRows].length;
-
-        while(count > 0){
-            int randRow = secureRand.nextInt(maxRows);
-            int randCol = secureRand.nextInt(maxCols);
-            if(!this.getBoard().getCell(randRow, randCol).hasBomb()){
-                this.getBoard().getCell(randRow, randCol).setBomb(true);
-                count--;
-            }
-        }
-
-    }
-
     //determine which cells adjacent to the specified cell have a bomb
     public int getAdjacentBombsCount(int row, int col){
 
@@ -133,6 +76,79 @@ public class GameModel {
         return count;
 
 
+    }
+
+    //set a given number of bombs on the board given a count
+    public void setBombs(int count){
+
+        //need to generate two random integers, the row value and the column value
+        SecureRandom secureRand = new SecureRandom();
+        int maxRows = this.getBoard().getCells().length;
+        int maxCols = this.getBoard().getCells()[maxRows].length;
+
+        while(count > 0){
+            int randRow = secureRand.nextInt(maxRows);
+            int randCol = secureRand.nextInt(maxCols);
+            if(!this.getBoard().getCell(randRow, randCol).hasBomb()){
+                this.getBoard().getCell(randRow, randCol).setBomb(true);
+                count--;
+            }
+        }
+
+    }
+
+    //get the numbers of bombs that the user has yet to flag
+    public int getUnflaggedBombsRemaining(){
+
+        int unflaggedBombs = 0;
+
+        for(int i = 0; i < this.getBoard().getCells().length; i++){
+            for(int j = 0; j < this.getBoard().getCells()[i].length; j++){
+                if(this.getBoard().getCell(i, j).hasBomb() && !this.getBoard().getCell(i, j).hasUserFlag()){
+                    unflaggedBombs++;
+                }
+            }
+        }
+
+        return unflaggedBombs;
+    }
+
+    //reset the board with the current row and column conditions
+    public void resetBoard(){
+
+        int row = this.getBoard().getCells().length;
+        int col = this.getBoard().getCells()[row].length;
+
+        Cell[][] cells = new Cell[row][col];
+        Board board = new Board(cells);
+        this.setBoard(board);
+    }
+
+    //reset the board with new row and column conditions
+    public void resetBoard(int row, int col){
+        Cell[][] cells = new Cell[row][col];
+        Board board = new Board(cells);
+        this.setBoard(board);
+    }
+
+    //get the bomb status of a particular cell
+    public boolean hasBomb(int row, int col){
+        return this.getBoard().getCell(row, col).hasBomb();
+    }
+
+    //get the flagged status of a particular cell
+    public boolean hasUserFlag(int row, int col){
+        return this.getBoard().getCell(row, col).hasUserFlag();
+    }
+
+    //set the bomb status of a particular cell
+    public void setBomb(int row, int col, boolean bomb){
+        this.getBoard().getCell(row, col).setBomb(bomb);
+    }
+
+    //set the flagged status of a particular cell
+    public void setUserFlag(int row, int col, boolean userFlag){
+        this.getBoard().getCell(row, col).setUserFlag(userFlag);
     }
 
     //get the game board
