a633ebb176ad58e727735029b8f3d263ea4b86bd
zach <ke3n1@proton.me>
2025-05-06 23:51:36 -0400

    comments

--- src/main/java/models/Board.java
+++ src/main/java/models/Board.java
@@ -1,25 +1,36 @@
 package models;
 
 import javax.swing.*;
+//a game is represented by a board, which contains a 2-d array of cells
+//TODO: determine if this should indeed extend jpanel. I don't think it should but IDK
+public class Board {
 
-public class Board extends JPanel {
+    //cells state
     private Cell[][] cells;
+
+    //constructors
     public Board(){
     }
     public Board(Cell[][] cells){
         this.cells = cells;
     }
 
+    //cells setter
     public void setCells(Cell[][] cells){
         this.cells = cells;
     }
+
+    //cells getter
     public Cell[][] getCells(){
         return this.cells;
     }
 
+    //individual cell getter
     public Cell getCell(int row, int col){
         return this.getCells()[row][col];
     }
+
+    //individual cell setter
     public void setCell(int row, int col, Cell cell){
         this.getCells()[row][col] = cell;
     }
--- src/main/java/models/Cell.java
+++ src/main/java/models/Cell.java
@@ -1,27 +1,40 @@
 package models;
 
+//cell model
+//a cell has two attributes -- whether it contains a bomb and whether a user has flagged it or not
 public class Cell {
+
+    //bomb and flag attributes
     private boolean bomb;
     private boolean userFlag;
+
+    //constructor
     public Cell(){
 
     }
+
+    //constructor
     public Cell(boolean bomb, boolean userFlag){
         this.bomb = bomb;
         this.userFlag = userFlag;
     }
 
+    //bomb status getter
     public boolean hasBomb(){
         return this.bomb;
     }
 
+    //flag status getter
     public boolean hasUserFlag(){
         return this.userFlag;
     }
 
+    //flag status setter
     public void setUserFlag(boolean userFlag){
         this.userFlag = userFlag;
     }
+
+    //bomb status setter
     public void setBomb(boolean bomb){
         this.bomb = bomb;
     }
