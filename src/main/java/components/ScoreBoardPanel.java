ec6112e306948a4cf438cacbcae5c33c4032e295
zach <ke3n1@proton.me>
2025-05-07 00:43:16 -0400

    initial definition of ScoreBoardPanel

--- src/main/java/components/ScoreBoardPanel.java
+++ src/main/java/components/ScoreBoardPanel.java
@@ -3,8 +3,45 @@ package components;
 import javax.swing.*;
 
 public class ScoreBoardPanel {
-    private JLabel userFlagsUsed;
+    private JLabel flagsSet;
     private JLabel bombsRemaining;
     private JButton gameStatusButton;
+
+    public ScoreBoardPanel(){
+
+    }
+
+    public void initFlagsSetLabel(){
+        this.flagsSet = new JLabel("0");
+
+    }
+    public JLabel getFlagsSetLabel(){
+        if(this.flagsSet == null){
+            initFlagsSetLabel();
+        }
+        return this.flagsSet;
+    }
+
+    public void initBombsRemainingLabel(){
+        this.bombsRemaining = new JLabel("0");
+
+    }
+    public JLabel getBombsRemainingLabel(){
+        if(this.bombsRemaining == null){
+            initBombsRemainingLabel();
+        }
+        return this.bombsRemaining;
+    }
+
+    public void initGameStatusButton(){
+        this.gameStatusButton = new JButton("status");
+    }
+    public JButton getGameStatusButton(){
+        if(this.gameStatusButton == null){
+            initGameStatusButton();
+        }
+        return this.gameStatusButton;
+    }
+
 
 }
