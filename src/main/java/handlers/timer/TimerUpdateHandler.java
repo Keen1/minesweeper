c1a0f95c0dfa73b6364e4c6e796f14144ba15105
zach <ke3n1@proton.me>
2025-05-08 01:15:29 -0400

    comments

--- src/main/java/handlers/timer/TimerUpdateHandler.java
+++ src/main/java/handlers/timer/TimerUpdateHandler.java
@@ -5,19 +5,29 @@ import controllers.GameController;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/*
+* timer update handler
+* updates the timer label every 1000ms (1sec) as defined by the timer instantiated in the scoreboard panel
+*/
+
 public class TimerUpdateHandler implements ActionListener {
 
+    //attributes
     private final GameController gameController;
 
+    //constructor
     public TimerUpdateHandler(GameController gameController){
         this.gameController = gameController;
     }
 
+
+    //every tick of the timer call the update function in the controller
     @Override
     public void actionPerformed(ActionEvent event){
         this.getGameController().updateTimer();
     }
 
+    //get the controller
     public GameController getGameController(){
         return this.gameController;
     }
