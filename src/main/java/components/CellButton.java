19f409a407ff47034393bc575c7bc598ca41762d
zach <ke3n1@proton.me>
2025-05-08 01:13:49 -0400

    comments

--- src/main/java/components/CellButton.java
+++ src/main/java/components/CellButton.java
@@ -2,17 +2,28 @@ package components;
 
 import javax.swing.*;
 
+/*
+*Cell button for front end. Tracks the location of the button via the row and column attribute while maintaining
+* JButton behavior through extension
+*/
 public class CellButton extends JButton {
+    //attributes
     private final int row;
     private final int column;
+
+    //constructor
     public CellButton(int row, int column){
+
         this.row = row;
         this.column = column;
     }
 
+    //get the row of the button
     public int getRow(){
         return this.row;
     }
+
+    //bet the column of the button
     public int getColumn(){
         return this.column;
     }
