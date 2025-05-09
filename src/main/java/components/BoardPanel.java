55aa691046dac508a0803395ce8d628b15ff0082
zach <ke3n1@proton.me>
2025-05-08 20:28:56 -0400

    comments and refactor

--- src/main/java/components/BoardPanel.java
+++ src/main/java/components/BoardPanel.java
@@ -1,24 +1,27 @@
 package components;
 
-import handlers.cells.CellMineImageHandler;
 
 import javax.swing.*;
 import java.awt.*;
-
+/*
+* the board panel class
+* holds the "board" of the game, all the cells that may or may not contain mines
+* the gui realizes the cells as buttons with listeners attached in the controller
+*/
 public class BoardPanel extends JPanel{
+    //static dimension for cells
     private static final Dimension CELL_DIM = new Dimension(25,25);
 
-
-    private static final String MINE_PATH = "/images/mine.png";
-    private static final String RED_FLAG_PATH = "/images/red_flag.png";
-
+    //variables for board attributes
     private int boardRows;
     private int boardColumns;
     private int mineCount;
 
+    //component variables
     private CellButton[][] cellButtons;
     private JPanel buttonPanel;
 
+    //default constructor
     public BoardPanel(){
         this.boardRows = 9;
         this.boardColumns = 9;
@@ -26,6 +29,7 @@ public class BoardPanel extends JPanel{
         initComponents();
     }
 
+    //overloaded constructor
     public BoardPanel(int boardRows, int boardColumns, int mineCount){
         this.boardRows = boardRows;
         this.boardColumns = boardColumns;
@@ -33,6 +37,7 @@ public class BoardPanel extends JPanel{
         initComponents();
     }
 
+    //initialize the components of the board panel
     private void initComponents(){
         setLayout(new BorderLayout());
         GridBagConstraints gbc = new GridBagConstraints();
@@ -51,7 +56,7 @@ public class BoardPanel extends JPanel{
     }
 
 
-
+    //change the size of the board
     public void changeBoardSize(int rows, int cols, int mineCount){
         this.setBoardRows(rows);
         this.setBoardColumns(cols);
@@ -59,10 +64,12 @@ public class BoardPanel extends JPanel{
         this.initCellButtons();
     }
 
-
+    //initialize the button panel
     private void initButtonPanel(){
         this.buttonPanel = new JPanel(new GridBagLayout());
     }
+
+    //get the button panel
     public JPanel getButtonPanel(){
         if(this.buttonPanel == null){
             initButtonPanel();
@@ -70,7 +77,7 @@ public class BoardPanel extends JPanel{
         return this.buttonPanel;
     }
 
-
+    //initialize the cell buttons
     private void initCellButtons(){
         this.cellButtons = new CellButton[this.getBoardRows()][this.getBoardColumns()];
         for(int i = 0; i < this.getBoardRows(); i++){
@@ -83,10 +90,12 @@ public class BoardPanel extends JPanel{
         }
     }
 
+    //set the cell buttons
     public void setCellButtons(CellButton[][] cellButtons){
         this.cellButtons = cellButtons;
     }
 
+    //get the cell buttons
     public CellButton[][] getCellButtons(){
 
         if(this.cellButtons == null){
@@ -96,28 +105,37 @@ public class BoardPanel extends JPanel{
         return this.cellButtons;
     }
 
+    //set the cell button's image icon
     public void setCellImageIcon(CellButton button, ImageIcon icon){
         button.setIcon(icon);
     }
 
+    //set the mine count
     public void setMineCount(int mineCount){
         this.mineCount = mineCount;
     }
+
+    //set the board rows
     public void setBoardRows(int boardRows){
         this.boardRows = boardRows;
     }
+
+    //set the board columns
     public void setBoardColumns(int boardColumns){
         this.boardColumns = boardColumns;
     }
 
+    //get the mine count
     public int getMineCount(){
         return this.mineCount;
     }
 
+    //get the board rows
     public int getBoardRows(){
         return this.boardRows;
     }
 
+    //get the board columns
     public int getBoardColumns(){
         return this.boardColumns;
     }
