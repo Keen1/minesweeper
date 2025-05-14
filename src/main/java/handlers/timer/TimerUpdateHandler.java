e9b995d7eba575fad4f703d20fcff0df312a3de3
zach <ke3n1@proton.me>
2025-05-14 00:08:55 -0400

    reverted back to class instead of record, caused issues with timer firing and updating timer label correctly.

--- src/main/java/handlers/timer/TimerUpdateHandler.java
+++ src/main/java/handlers/timer/TimerUpdateHandler.java
@@ -9,13 +9,24 @@ import java.awt.event.ActionListener;
 * timer update handler
 * updates the timer label every 1000ms (1sec) as defined by the timer instantiated in the scoreboard panel
 */
+public class TimerUpdateHandler implements ActionListener {
+    private final GameController gameController;
+    public TimerUpdateHandler(GameController gameController){
+        this.gameController = gameController;
+    }
 
-public record TimerUpdateHandler(GameController gameController) implements ActionListener {
+    //constructor
+
 
     //every tick of the timer call the update function in the controller
     @Override
     public void actionPerformed(ActionEvent event) {
-        this.gameController().updateTimer();
+        this.getGameController().updateTimer();
+    }
+
+
+    public GameController getGameController(){
+        return this.gameController;
     }
 
 }
