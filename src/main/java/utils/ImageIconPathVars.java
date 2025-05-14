10fe2f324bbed9a375dc5a90962cbabb9308875d
zach <ke3n1@proton.me>
2025-05-13 23:25:51 -0400

    refactored for warnings.

--- src/main/java/utils/ImageIconPathVars.java
+++ src/main/java/utils/ImageIconPathVars.java
@@ -3,8 +3,8 @@ package utils;
 public enum ImageIconPathVars {
     FLAG("flag", "/images/red_flag_24.png"),
     MINE("mine", "/images/mine.png");
-    private String code;
-    private String path;
+    private final String code;
+    private final String path;
 
     ImageIconPathVars(String code, String path){
         this.code = code;
