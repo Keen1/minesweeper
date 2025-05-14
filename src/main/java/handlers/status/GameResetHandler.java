bc1787b80acbb4501580cab0e614d226400c750d
zach <ke3n1@proton.me>
2025-05-13 23:18:36 -0400

    comments

--- src/main/java/handlers/status/GameResetHandler.java
+++ src/main/java/handlers/status/GameResetHandler.java
@@ -4,20 +4,28 @@ import controllers.GameController;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+/*
+* game reset handler
+* resets the game when the status button is clicked
+*/
 public class GameResetHandler implements ActionListener {
+    //controller variable
     private final GameController gameController;
 
+    //constructor
     public GameResetHandler(GameController gameController){
         this.gameController = gameController;
     }
 
+    //override for action performed
+    //reset the board and set the image icon
     @Override
     public void actionPerformed(ActionEvent event){
         this.getController().resetBoard();
         this.getController().setSmilingImageIcon();
     }
 
+    //get the controller
     public GameController getController(){
         return this.gameController;
     }
