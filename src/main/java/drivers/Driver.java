8295e84b312d0d297cabf14b243a62c3376fd624
zach <ke3n1@proton.me>
2025-05-13 23:05:10 -0400

    comments, spacing

--- src/main/java/drivers/Driver.java
+++ src/main/java/drivers/Driver.java
@@ -1,11 +1,17 @@
 package drivers;
 
 import components.GameFrame;
-
 import javax.swing.*;
 
+/*
+* driver class
+* entry point for the program*/
 public class Driver {
+
+    //main
     public static void main(String[] args){
+
+        //set the game frame and show the frame
         SwingUtilities.invokeLater( () -> {
             GameFrame frame = new GameFrame();
             frame.showFrame();
