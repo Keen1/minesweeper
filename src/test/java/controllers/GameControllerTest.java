b19bc1cb903d1b430da0bafa9ebd8b2195a51cf6
zach <ke3n1@proton.me>
2025-05-15 20:47:29 -0400

    added game controller test class

--- src/test/java/controllers/GameControllerTest.java
+++ src/test/java/controllers/GameControllerTest.java
@@ -1,4 +1,15 @@
 package controllers;
 
+import components.GameFrame;
+import org.junit.jupiter.api.BeforeAll;
+
 public class GameControllerTest {
+    private static GameFrame gameFrame;
+
+    @BeforeAll
+    public static void setup(){
+        gameFrame = new GameFrame();
+    }
+
+
 }
